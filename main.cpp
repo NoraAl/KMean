fe513eb7628b2235e77879fecea58e7232ad332c
Nora Alosily <nemseo@gmail.com>
2018-04-19 10:36:17 -0500

    2nd:     - Initilizing centroids randomly.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 
 using namespace std;
 
+
 typedef int type;
 enum MEASURE{Euclidean, Manhattan};
 struct Point{
@@ -20,8 +21,10 @@ struct Point{
     int id;
     int cluster;
 };
+static vector <Point> points;
 
-void generateRandom(vector<Point>& points, type min = 1, type max = 100, int num = 100 );
+void generateRandom(vector<Point>& points, type minX, type maxX,int num);
+void generateRandom(vector<Point>& points, type minX, type maxX,int num,  type minY, type maxY );
 vector<vector <Point> > cluster(vector <Point>, int k, MEASURE measure = Euclidean);
 
 /****************************
@@ -36,9 +39,9 @@ vector<vector <Point> > cluster(vector <Point>, int k, MEASURE measure = Euclide
  ****************************/
 int main() {
     try{
-
-        vector<Point> points;
-        generateRandom(points);
+        srand(time(nullptr));
+        //vector<Point> points;
+        generateRandom(points,1,100,100);
 
         int clusterCounts[]={2,3};
         cluster(points, 2);
@@ -64,28 +67,116 @@ int main() {
  *
  ****************************/
 
-void generateRandom(vector<Point>& points, type min, type max, int num){
-    srand(time(nullptr)); // use current time as seed for random generator
+inline int getRandom(type min, type max ){
 
+    return (rand()%(max-min+1))+min;
+}
+
+void generateRandom(vector<Point>& points, type minX, type maxX, int num,  type minY, type maxY ){
     Point point;
     while (num){
-        point.x = (rand()%(max-min+1))+min;
-        point.y = (rand()%(max-min+1))+min;
+        point.x = getRandom(minX,maxX);// (rand()%(maxX-minX+1))+minX;
+        point.y = getRandom(minY,maxY);//(rand()%(maxY-minY+1))+minY;
         points.push_back(point);
         num--;
     }
 }
 
-void initialCenters( vector<Point>& centroids, int k ){
-    generateRandom(centroids, 1, 100, k);
+void generateRandom(vector<Point>& points, type minX, type maxX, int num ){
+    generateRandom(points,minX,maxX,num,minX,maxX);
 }
 
-inline bool adjust(vector<Point>& points, vector<Point>& centroids){
-    vector <Point> oldcentroids = centroids;
-    //adjust the labels and the centorids
-    //
-    if ()
 
+type getMaxX(){
+    type max = points.at(0).x;
+    for (auto point: points){
+        if (point.x > max)
+            max = point.x;
+    }
+    return max;
+}
+
+type getMaxY(){
+    type max = points.at(0).y;
+    for (auto point: points){
+        if (point.y > max)
+            max = point.y;
+    }
+    return max;
+}
+
+type getMinX(){
+    type min = points.at(0).x;
+    for (auto point: points){
+        if (point.x < min)
+            min = point.x;
+    }
+    return min;
+}
+
+type getMinY(){
+    type min = points.at(0).y;
+    for (auto point: points){
+        if (point.y < min)
+            min = point.y;
+    }
+    return min;
+}
+
+void initialCenters( vector<Point>& centroids, int k ){
+    generateRandom(centroids, 1,100, k);
+
+    for (auto p: centroids){
+        cout <<p.x<<","<<p.y<<endl;
+    }
+    cout <<"==============="<<endl;
+    auto fun = [&centroids](int index){
+
+        for (int i =index; i< centroids.size()-1; i++) {
+            for (int j = i + 1; j < centroids.size(); j++) {
+                cout << "comparing"<<i<<","<<j<<" ("<<centroids[i].x<<","<<centroids[i].y<<"),("<<centroids[j].x<<","<<centroids[j].y<<")";
+                if (centroids[i].x == centroids[j].x)
+                    if (centroids[i].y == centroids[j].y){
+                        cout <<" ==\n";
+                        return i;
+
+                    }
+                cout <<" √\n";
+            }
+        }
+        return -1;
+    };
+
+    int index = fun(0);
+
+    while(index >= 0){
+        //cout << centroids[index].x<<","<<centroids[index].y<<endl;
+        index = fun(index);
+        centroids[index].x = getRandom(1,100);
+        centroids[index].y = getRandom(1,100);
+
+    }
+
+    cout <<"==============="<<endl;
+    for (auto p: centroids){
+        cout <<p.x<<","<<p.y<<endl;
+    }
+
+
+
+
+}
+
+inline bool checkCentroids(vector<Point> &points, vector<Point> &centroids){
+    vector <Point> oldcentroids = centroids;
+    //checkCentroids the labels and the centorids
+    //
+    return true;
+
+
+}
+
+inline void updateLabels(vector<Point> &points, vector<Point> &centroids){
 
 }
 
@@ -94,7 +185,13 @@ vector<vector <Point> > cluster(vector <Point> points, int k, MEASURE measure ){
     vector<Point> centroids;
 
     //todo: now it is random, do it smartly
+
     initialCenters(centroids, k);
+    do{
+        updateLabels(points, centroids);
+    }
+    while (checkCentroids(points, centroids));
+
 
     return clusters;
 }
